d17129155d6ee698535f1790afecd3e53cc44b34
Максим <90149954+PTyneu@users.noreply.github.com>
2022-10-22 16:30:31 +0300

    Add files via upload

---
+++ SrGAN/loss.py
@@ -0,0 +1,16 @@
+from turtle import forward
+import torch.nn as nn
+from torchvision.models import vgg19
+import config
+
+class VGGLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.vgg = vgg19(pretrained=True).features[:36].eval().to(config.DEVICE)
+        self.loss = nn.MSELoss()
+        for param in self.vgg.parameters():
+            param.requires_grad = False
+    def forward(self, input, target):
+        vgg_input_features = self.vgg(input)
+        vgg_target_parameters = self.vgg(target)
+        return self.loss(vgg_input_features, vgg_target_parameters)
